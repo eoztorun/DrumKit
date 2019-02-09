3f2185251dd4b1f6f2b4b12b063db7718b870241
Ecenur <eoztorun@gmail.com>
2019-02-09 13:19:12 -0500

    sounds were added to the buttons.

--- index.js
+++ index.js
@@ -3,10 +3,50 @@ var drumButtons = document.querySelectorAll(".drum");
 
 //add "click" event listener to all the buttons
 for (var i = 0; i < drumButtons.length; i++) {
-  drumButtons[i].addEventListener("click", handleClick);
+  drumButtons[i].addEventListener("click", handleClick)
 }
 
 //function to handle what happens on click event
 function handleClick() {
-  alert("I got clicked");
+  var buttonInnerHTML = this.textContent;
+
+  switch (buttonInnerHTML) {
+    case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+      break;
+
+    case "a":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+      break;
+
+    case "s":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+      break;
+
+    case "d":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+      break;
+
+    case "j":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+      break;
+
+    case "k":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+      break;
+
+    case "l":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+      break;
+
+    default:
+        console.log(buttonInnerHTML);
+  }
 }
